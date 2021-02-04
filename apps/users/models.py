07ac8ad27e0d3ff4c46dbabcf0c2b373e4cb3d3d
etobaza <zonbon@icloud.com>
2021-02-05 01:58:44 +0300

    feat(BC-1): settings fix

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Bloguser(models.User):
+    birthdate = models.DateField()
+    bio = models.CharField()
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from serializers import UserSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated ]
 
 # Create your views here.
