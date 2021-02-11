ced69a297b313b9cb4db11d285e86e3de36d0eb5
etobaza <zonbon@icloud.com>
2021-02-11 23:14:25 +0300

    feat(BC-1): settings fix

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Bloguser(models.User):
+
+class BlogUser(User):
     birthdate = models.DateField()
-    bio = models.CharField()
+    bio = models.TextField()
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+from .models import BlogUser
+
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ["all"]
+        model = BlogUser
+        fields = "__all__"
