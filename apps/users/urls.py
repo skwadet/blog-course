74a24f478f7726b7bbee854da80cfb8812724f48
etobaza <zonbon@icloud.com>
2021-02-11 23:36:46 +0300

    feat(BC-1): settings fix

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from views import UserViewSet
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register("users", UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("api/", include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
