380ce6da131af137435b188f45d7eab10bdf98a9
etobaza <zonbon@icloud.com>
2021-02-12 02:16:26 +0300

    feat(BC-1): settings fix

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,3 +5,8 @@ from django.contrib.auth.models import User
 class BlogUser(User):
     birthdate = models.DateField()
     bio = models.TextField()
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    user = models.ForeignKey('BlogUser')
+    timestamp = models.DateTimeField()
