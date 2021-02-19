4a64d1f3d56f75a6aff735b5e2b659fdacd72d22
etobaza <zonbon@icloud.com>
2021-02-19 19:58:58 +0300

    feat(BC-1): settings fix

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey('BlogUser')
+    timestamp = models.DateTimeField()
--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Comment
+from serializers import CommentSerializer
 
-# Create your views here.
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects
+    serializer_class = CommentSerializer
